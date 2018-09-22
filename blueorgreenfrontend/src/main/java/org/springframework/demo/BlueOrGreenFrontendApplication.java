5c991230a7005ac9a7b7ca5b3613c90004e652d6
Cora Iberkleid <ciberkleid@pivotal.io>
2018-09-22 12:18:20 -0400

    Remove type cookie (configurable)

--- blueorgreenfrontend/src/main/java/org/springframework/demo/BlueOrGreenFrontendApplication.java
+++ blueorgreenfrontend/src/main/java/org/springframework/demo/BlueOrGreenFrontendApplication.java
@@ -2,20 +2,20 @@ package org.springframework.demo;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import javax.servlet.http.Cookie;
+import java.util.regex.Pattern;
+
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.RequestEntity;
 import org.springframework.util.LinkedMultiValueMap;
@@ -28,21 +28,31 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 @RestController
 public class BlueOrGreenFrontendApplication {
+	private final Logger log = LoggerFactory.getLogger(BlueOrGreenFrontendApplication.class);
 
 	@Autowired
 	RestTemplate rest;
+
+	@Value("${removeTypeCookie:true}")
+	private boolean removeTypeCookie;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlueOrGreenFrontendApplication.class, args);
 	}
 
+	private final static Pattern pattern = Pattern.compile(" *; *");
+
 	@RequestMapping("/color")
 	public String color(HttpServletRequest request) throws URISyntaxException {
 		String cookies = request.getHeader("cookie");
 		MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
-		if(cookies != null) {
-			headers.set("cookie", cookies);
+		if (removeTypeCookie) {
+			cookies = removeCookie(cookies, "type");
 		}
+		if (cookies != null && cookies.length() > 0) {
+				headers.set("cookie", cookies);
+		}
+
 		RequestEntity requestEntity = new RequestEntity(headers, HttpMethod.GET, new URI("http://blueorgreengateway/blueorgreen"));
 		return rest.exchange(requestEntity, String.class).getBody();
 	}
@@ -56,4 +66,23 @@ public class BlueOrGreenFrontendApplication {
 			return new RestTemplateBuilder().build();
 		}
 	}
+
+	private String removeCookie(String cookies, String cookieName) {
+		String newCookies = "";
+		if(cookies != null) {
+			log.info("Got cookies: {}", cookies);
+			String[] tokens = pattern.split(cookies);
+			for(String token : tokens) {
+				if (!token.substring(0, token.indexOf("=")).equals(cookieName)) {
+					if (newCookies.length() > 0) {
+						newCookies = newCookies.concat(" ; ");
+					}
+					newCookies = newCookies.concat(token);
+				}
+			}
+			log.info("Processed cookies: {}", newCookies);
+		}
+		return newCookies;
+	}
+
 }
