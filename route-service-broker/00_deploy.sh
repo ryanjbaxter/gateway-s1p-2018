e35aa9270659b1aab93fc1ccd4a9faf9cd7398d0
Cora Iberkleid <ciberkleid@pivotal.io>
2018-10-15 19:11:30 -0400

    unique values for techfest

--- route-service-broker/00_deploy.sh
+++ route-service-broker/00_deploy.sh
@@ -1,16 +1,16 @@
 # CLEANUP
 
-cf urs cfapps.io subscription-gateway --hostname cool-app -f
-cf unmap-route cool-app cfapps.io --hostname cool-app
+cf urs cfapps.io subscription-gateway --hostname techfest-cool-app -f
+cf unmap-route cool-app cfapps.io --hostname techfest-cool-app
 
-cf urs cfapps.io subscription-gateway --hostname appgateway -f
-#cf unmap-route appgateway cfapps.io --hostname appgateway
+cf urs cfapps.io subscription-gateway --hostname techfest-bluegreengateway -f
+cf unmap-route bluegreengateway cfapps.io --hostname techfest-bluegreengateway
 
 cf ds subscription-gateway -f
 
-cf delete-service-broker route-service -f
+cf delete-service-broker techfest-route-service -f
 
-#cf d route-service-broker -f
+#cf d techfest-route-service-broker -f
 #cf ds route-service-mongodb -f
 #cf ds route-service-redis -f
 
@@ -21,20 +21,23 @@ cf delete-orphaned-routes -f
 #cd ~/workspace/route-service-broker-thumbnail
 #cf push
 
-cd ~/workspace/route-service-broker
+#cd ~/workspace/gateway-s1p-2018/cool-app
+#cf push
+
+cd ~/workspace/gateway-s1p-2018/route-service-broker
 ./gradlew assemble
-#cf cs mlab sandbox route-service-mongodb
-#cf cs rediscloud 30mb route-service-redis
+cf cs mlab sandbox route-service-mongodb
+cf cs rediscloud 30mb route-service-redis
 cf push
 
 # RECONFIGURE
 
-cf create-service-broker route-service admin supersecret https://route-service-broker.cfapps.io --space-scoped
+cf create-service-broker techfest-route-service admin supersecret https://techfest-route-service-broker.cfapps.io --space-scoped
 
-cf cs route-service standard subscription-gateway
+cf cs techfest-route-service standard subscription-gateway
 
-cf map-route cool-app cfapps.io --hostname cool-app
-cf brs cfapps.io subscription-gateway --hostname cool-app
+cf map-route cool-app cfapps.io --hostname techfest-cool-app
+cf brs cfapps.io subscription-gateway --hostname techfest-cool-app
 
-#cf map-route appgateway cfapps.io --hostname appgateway
-cf brs cfapps.io subscription-gateway --hostname appgateway
+#cf map-route bluegreengateway cfapps.io --hostname techfest-bluegreengateway
+cf brs cfapps.io subscription-gateway --hostname techfest-bluegreengateway
