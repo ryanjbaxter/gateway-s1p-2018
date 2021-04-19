6a2410982990dac08e6da183d21644dd2a3ac9b4
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2021-04-19 15:24:26 -0700

    imports :(

--- blueorgreenservice/src/main/java/org/springframework/demo/ColorController.java
+++ blueorgreenservice/src/main/java/org/springframework/demo/ColorController.java
@@ -1,7 +1,6 @@
 package org.springframework.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
