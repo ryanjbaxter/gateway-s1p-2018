b487fdfe0af4c4da2569e0c7fe741a493d8e84ce
Cora Iberkleid <ciberkleid@gmail.com>
2021-05-05 01:58:37 -0400

    remove mentions of config server and eureka server as project modules from scripts

--- build-images.sh
+++ build-images.sh
@@ -4,5 +4,3 @@ pack build authgateway:pk --path ./authgateway/target/authgateway-0.0.1-SNAPSHOT
 pack build blueorgreenfrontend:pk --path ./blueorgreenfrontend/target/blueorgreenfrontend-0.0.1-SNAPSHOT.jar
 pack build blueorgreengateway:pk --path ./blueorgreengateway/target/blueorgreengateway-0.0.1-SNAPSHOT.jar
 pack build blueorgreenservice:pk --path ./blueorgreenservice/target/blueorgreen-0.0.1-SNAPSHOT.jar
-pack build configserver:pk --path ./configserver/target/configserver-0.0.1-SNAPSHOT.jar
-pack build eurekaserver:pk --path ./eurekaserver/target/eurekaserver-0.0.1-SNAPSHOT.jar
