be1834cdb0fca3bf34bb6f0c1a23cef4f2253002
Cora Iberkleid <ciberkleid@gmail.com>
2021-05-05 01:19:19 -0400

    finish removing configserver and eureka and configuring spring cloud CLI

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 mkdir -p logs
 
-cd configserver
-nohup ./mvnw spring-boot:run > ../logs/configserver.out 2>&1 </dev/null &
+nohup spring cloud configserver > logs/configserver.out 2>&1 </dev/null &
 
-cd ../eurekaserver
-nohup ./mvnw spring-boot:run > ../logs/eurekaserver.out 2>&1 </dev/null &
+sleep 10
 
-sleep 20
+nohup spring cloud eureka > logs/eureka.out 2>&1 </dev/null &
 
-cd ../blueorgreenservice
+sleep 10
+
+cd blueorgreenservice
 nohup ./mvnw spring-boot:run -Dspring-boot.run.profiles=blue > ../logs/blue.out 2>&1 </dev/null &
 nohup ./mvnw spring-boot:run -Dspring-boot.run.profiles=green > ../logs/green.out 2>&1 </dev/null &
 nohup ./mvnw spring-boot:run -Dspring-boot.run.profiles=slowgreen > ../logs/slowgreen.out 2>&1 </dev/null &
